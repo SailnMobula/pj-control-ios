f7ecfaaf4b17561b8358375a6b7a9170a6a470c1
Julian Kramer <Julian.Kramer@EXXETA.com>
2020-06-05 00:04:59 +0200

    Initial Commit

---
+++ PJ Monitor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PJ Monitor
+//
+//  Created by Kramer, Julian on 05.06.20.
+//  Copyright © 2020 Julian Kramer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
